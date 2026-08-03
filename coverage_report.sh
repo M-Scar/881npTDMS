b5dc8e9e0b485d0b5eaaa1001f1f423a9f51f9d9
Mifchell <mwm31495@gmail.com>
2026-08-03 18:51:07 -0400

    Updated Ignore and added coverage script

---
+++ coverage_report.sh
@@ -0,0 +1,17 @@
+if [ -d "htmlcov" ]; then
+    echo "Removing htmlcov Dir"
+    rm -rf htmlcov
+fi
+
+if [ -f ".coverage" ]; then
+    echo "Removing Coverage File"
+    coverage erase
+fi
+
+echo "Running Tests With Coverage"
+coverage run --source=nptdms -m unittest discover
+
+echo "Generating HTML Coverage"
+coverage html --omit="*/nptdms/tests/*,*/nptdms/test/*,*/nptdms/export/*,*/nptdms/version.py,*/nptdms/__init__.py"
+
+echo "Done"
