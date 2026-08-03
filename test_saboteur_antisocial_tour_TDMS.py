e0087174addc731cac8335de69643645f493d798
Michael Scarpitta <mss6102@psu.edu>
2026-08-02 23:37:45 -0400

    fixed resource warning. labview exe now closes after 10 seconds, and closes the tdms as well.

--- test_saboteur_antisocial_tour_TDMS.py
+++ test_saboteur_antisocial_tour_TDMS.py
@@ -13,7 +13,8 @@ class TestTDMS(unittest.TestCase):
         exe_path = os.path.join(script_dir, "helperPrograms", "advancedOpenNoClose.exe")   
         tdmsFilePath = os.path.join(script_dir, "helperPrograms", "advancedOpen.tdms")  
         print(exe_path)     
-        pro = subprocess.Popen([exe_path], stdout=subprocess.PIPE, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
+        pro = subprocess.Popen([exe_path], creationflags=subprocess.CREATE_NEW_PROCESS_GROUP,
+                               stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         time.sleep(5) #possible to get a signal from labview that the file is indeed open?
         print("starting a read attempt")
         tdmsfile = nptdms.TdmsFile(tdmsFilePath, read_metadata_only=False, keep_open=False)        
